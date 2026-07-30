ced1e29e9666f4c9e8bdcb8a0936e963fd2a5289
VELTR0 <124514937+VELTR0@users.noreply.github.com>
2026-07-30 22:40:12 +0200

    Make Soullocke integration optional

--- Models/AppConfig.cs
+++ Models/AppConfig.cs
@@ -3,13 +3,14 @@ namespace SoulBuddy.Models;
 public sealed class AppConfig
 {
     public required string PartyJsonPath { get; init; }
-    public required string SessionId { get; init; }
-    public required string PlayerId { get; init; }
-    public required string TeamName { get; init; }
-    public required string PlayerName { get; init; }
-    public required string AuthToken { get; init; }
+    public string SessionId { get; init; } = string.Empty;
+    public string PlayerId { get; init; } = "player1";
+    public string TeamName { get; init; } = string.Empty;
+    public string PlayerName { get; init; } = string.Empty;
+    public string AuthToken { get; init; } = string.Empty;
 
     public int RunNumber { get; init; } = 1;
     public int PollIntervalMilliseconds { get; init; } = 1000;
     public bool DryRun { get; init; } = true;
+    public bool SoullockeEnabled { get; init; }
 }
