3be576966efb608c11db3bb9f3923f51acc5c5b5
VELTR0 <124514937+VELTR0@users.noreply.github.com>
2026-07-31 02:29:48 +0200

    Add Phase 2 Pokemon presentation fields

--- Models/PartyModels.cs
+++ Models/PartyModels.cs
@@ -46,6 +46,24 @@ public sealed class PartyPokemon
     [JsonPropertyName("isEgg")]
     public bool IsEgg { get; init; }
 
+    [JsonPropertyName("nature")]
+    public string Nature { get; init; } = string.Empty;
+
+    [JsonPropertyName("ability")]
+    public string Ability { get; init; } = string.Empty;
+
+    [JsonPropertyName("isFemale")]
+    public bool IsFemale { get; init; }
+
+    [JsonPropertyName("isGenderless")]
+    public bool IsGenderless { get; init; }
+
+    [JsonPropertyName("isShiny")]
+    public bool IsShiny { get; init; }
+
+    [JsonPropertyName("pokeball")]
+    public int Pokeball { get; init; }
+
     [JsonPropertyName("hp")]
     public PokemonHp Hp { get; init; } = new();
 }
