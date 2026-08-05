39c9d0b220d74b665997f3a746cb086a76f2ffc2
VELTR0 <124514937+VELTR0@users.noreply.github.com>
2026-08-06 00:21:59 +0200

    Add Nuzlocke rule event model

---
+++ Models/NuzlockeRuleEvent.cs
@@ -0,0 +1,23 @@
+namespace SoulBuddy.Models;
+
+public enum NuzlockeRuleEventType
+{
+    PokemonKnockedOut,
+    CatchableEncounter,
+    CatchSucceeded,
+    CatchFailed
+}
+
+public sealed class NuzlockeRuleEvent : EventArgs
+{
+    public required NuzlockeRuleEventType Type { get; init; }
+    public required DateTimeOffset OccurredAt { get; init; }
+    public required int SpeciesId { get; init; }
+    public required string SpeciesName { get; init; }
+    public string? Nickname { get; init; }
+    public string LocationName { get; init; } = "Unbekannter Ort";
+    public int? LocationId { get; init; }
+    public long Pid { get; init; }
+    public bool IsShiny { get; init; }
+    public bool IsFirstEncounter { get; init; }
+}
