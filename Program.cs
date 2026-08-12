8d52c5d2a0c2c7e0b44b9008b5fdd82ae0f622b0
VELTR0 <124514937+VELTR0@users.noreply.github.com>
2026-08-12 13:27:27 +0200

    Prevent duplicate SoulBuddy instances

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ internal static class Program
     [STAThread]
     public static void Main(string[] args)
     {
+        using var singleInstance = new Mutex(
+            initiallyOwned: true,
+            name: @"Local\SoulBuddy.SingleInstance",
+            createdNew: out var createdNew);
+
+        if (!createdNew)
+            return;
+
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
     }
