4b2499e90fef5ab39e72d257b8330d240065cc19
VELTR0 <124514937+VELTR0@users.noreply.github.com>
2026-08-16 14:11:17 +0200

    Add tracker client factory

---
+++ Services/TrackerClientFactory.cs
@@ -0,0 +1,13 @@
+using SoulBuddy.Models;
+
+namespace SoulBuddy.Services;
+
+public static class TrackerClientFactory
+{
+    public static ITrackerClient Create(HttpClient httpClient, AppConfig config) =>
+        config.TrackerProvider switch
+        {
+            TrackerProvider.SoullockeVercel => new VercelSoullockeClient(httpClient, config),
+            _ => new SoullockeClient(httpClient, config)
+        };
+}
