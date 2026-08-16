45509df0e917d5cb973dfa7733bbc2b8226823a9
VELTR0 <124514937+VELTR0@users.noreply.github.com>
2026-08-16 19:28:34 +0200

    Log tracker provider initialization

--- Services/TrackerClientFactory.cs
+++ Services/TrackerClientFactory.cs
@@ -4,10 +4,19 @@ namespace SoulBuddy.Services;
 
 public static class TrackerClientFactory
 {
-    public static ITrackerClient Create(HttpClient httpClient, AppConfig config) =>
-        config.TrackerProvider switch
+    public static ITrackerClient Create(HttpClient httpClient, AppConfig config)
+    {
+        DiagnosticLog.StartSession($"tracker provider={config.TrackerProvider}");
+        DiagnosticLog.Info(
+            "TrackerFactory",
+            $"Creating tracker client: provider={config.TrackerProvider}; " +
+            $"enabled={config.SoullockeEnabled}; session='{config.SessionId}'; " +
+            $"player='{config.PlayerName}'.");
+
+        return config.TrackerProvider switch
         {
             TrackerProvider.SoullockeVercel => new VercelTrackerClient(httpClient, config),
             _ => new SoullockeDotComTrackerClient(httpClient, config)
         };
+    }
 }
